a15586b2169b39f342dae950662f2ceec70a6115
MrJellyBeans <52618902+MrJellyBeans@users.noreply.github.com>
2021-12-04 10:48:42 +0200

    added couple of awesome stuff

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
-opening(`،يوم سعيد
+bot(`،يوم سعيد
 مرحبا! بكم في سي أس مينا لتأجير السيارات
 كيف يمكن أن أساعدك اليوم؟`)
 function output(input) {
@@ -21,10 +21,11 @@ function output(input) {
 
   let text = input
   text = text
-    .replace(/(\?|؟|!|!|\*)/g, "")
-    .replace(/(  )/g, " ")
+    .replace(/(\?|؟|!|!|\*|في |ال)/g, "")
+    .replace(/ +/g, " ")
     .replace(/(كيفك|كيف الحال)/g, "كيف الحياة")
     .replace(/(أ|إ|آ)/g, "ا")
+    .replace(/بدي/g, "اريد ان")
     .replace(/(شو|وش|مذا|ايش)/gi, "ماذا")
     .replace(/(عندكم|عندكو|لديكم|عندك)/g,"لديك")
     .replace(/(نتي)/g, "نت");
@@ -34,8 +35,35 @@ function output(input) {
     product = compare(prompts, replies, text);
   } else if (text.match(/شكرا/gi)) {
     product = "العفو"
+  } else if (text.match(/اريد ان استاجر/gi)) {
+    let foundcar
+    let filteredLength = arrayMatch(listNames(cars), text.split(/ +/g)).length
+     if(filteredLength >= 1){
+
+
+        if (cars.filter(x => x.name.includes(arrayMatch(listNames(cars), text.split(/ +/g))[0])).length == 1 ){
+      foundcar = cars.filter(x => x.name.includes(arrayMatch(listNames(cars), text.split(/ +/g))[0]))
+      product = `wewهل تريد أن تستأجر السياره ${foundcar[0].name}, مديل ${foundcar[0].model}, بسعر ${foundcar[0].cost} د.أ بليوم؟`
+
+      } else if (arrayMatch(listModels(cars), text.split(/ +/g)).length >= 1){
+       foundcar = cars.filter(x => x.name.includes(arrayMatch(listNames(cars), text.split(/ +/g))[0]))
+       product = `هل تريد أن تستأجر السياره ${foundcar[0].name}, مديل ${foundcar[0].model}, بسعر ${foundcar[0].cost} د.أ بليوم؟`
+
+      } else if (cars.filter(x => x.name.includes(arrayMatch(listNames(cars), text.split(/ +/g))[0])).length > 1){
+        let carList = cars.filter(x => x.name.includes(arrayMatch(listNames(cars), text.split(/ +/g))[0]))
+        console.log(carList)
+        carList = carList.map(x => `    #${carList.indexOf(x)+1} ${x.name} مديل  ${x.model} سنت ${x.year}`)
+        .join("\n")
+        product = `:لدينا اكثر من سيارة لديها الإسم ${arrayMatch(listNames(cars), text.split(/ +/g))[0]} و هم
+        ${carList}`
+      }
+
+
+    } 
   } else if (text.match(/ماذا لديك سيارات/g)){
     product = listCars(cars) 
+  } else if (arrayMatch(listNames(cars), text.split(/ +/g)).length >= 1){
+product = "console"
   } else {
     // If all else fails: random alternative
     product = alternative[Math.floor(Math.random() * alternative.length)];
@@ -91,11 +119,21 @@ function addChat(input, product) {
   // Fake delay to seem "real"
   setTimeout(() => {
     botText.innerText = `${product}`;
-  }, 2000
+  }, 1500
   )
 
 }
-function opening(product) {
+function user(input) {
+  const messagesContainer = document.getElementById("messages");
+
+  let userDiv = document.createElement("div");
+  userDiv.id = "user";
+  userDiv.className = "user response";
+  userDiv.innerHTML = `<img src="user.png" class="avatar"><span>${input}</span>`;
+  messagesContainer.appendChild(userDiv);
+  messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+}
+function bot(product) {
   const messagesContainer = document.getElementById("messages");
   let botDiv = document.createElement("div");
   let botImg = document.createElement("img");
@@ -119,7 +157,39 @@ function opening(product) {
 
 function listCars(cars){
   let list = cars
-  .map(row => `${row.name} -  ${row.model} - ${row.year}`)
+  .map(row => `${row.name} مديل  ${row.model} سنت ${row.year}`)
   .join("\n")
   return list
+}
+function listNames(cars){
+  let list = cars
+  .map(row => row.name)
+  return list
+}
+function listModels(cars){
+  let list = cars
+  .map(row => row.model)
+  return list
+}
+function listYears(cars){
+  let list = cars
+  .map(row => row.year)
+  return list
+}
+function resort(cars){
+  let list = cars
+  .map(row => Object.values(row))
+  return list
+}
+function arrayMatch(arr1, arr2) {
+  var arr = [];
+  for(var i=0 ; i<arr1.length ; ++i) {
+    for(var j=0 ; j<arr2.length ; ++j) {
+      if(arr1[i] == arr2[j]) { 
+        arr.push(arr1[i]);
+      }
+    }
+  }
+   
+  return arr;  
 }
